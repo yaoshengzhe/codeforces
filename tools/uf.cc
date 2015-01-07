ecafdaaf0bc77ed659b24ec6a11784b1a965a6cc
Shengzhe Yao <shengzhe@google.com>
2015-01-06 16:47:49 -0800

    add union-find implementation in tools

---
+++ tools/uf.cc
@@ -0,0 +1,20 @@
+// A union-find algorithm implementation
+
+int uf_find(int *uf, int x) {
+    while (uf[x] != x) {
+        int parent = uf[x];
+        uf[x] = uf[parent];
+        x = parent;
+    }
+    return x;
+}
+
+void uf_union(int *uf, int x, int y) {
+    int rootx = uf_find(uf, x);
+    int rooty = uf_find(uf, y);
+    if (rootx < rooty) {
+        uf[rooty] = rootx;
+    } else {
+        uf[rootx] = rooty;
+    }
+}
